17722b2041a53e7fbdd95cf11783ce2d9e1ba756
ricky.gummadi <ricky.gummadi>
2023-07-19 08:56:28 +1200

    fixed bugs

--- src/az-eventgrid-private-endpoints/Infra/Bicep/deploy.sh
+++
@@ -1,6 +0,0 @@
-az deployment sub create --location eastus --name 'private-eventgrid-endpoint-deployment' --template-file main.bicep --parameters `
-  location='eastus' `
-  vnetName='myVNet' `
-  bastionHostName='PrivateEventGridBastionHost' `
-  eventGridTopicName='PrivateEventGridTopic12' `
-  privateEndpointName='PrivateEventGridTopic12PrivateEndpoint'
